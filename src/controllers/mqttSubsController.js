d975d4ee0b4131999b16ad904e2350e719c242f5
zscaiosi <zscaio.si@gmail.com>
2017-07-05 21:46:08 -0300

    Update de estado da maquina

--- src/controllers/mqttSubsController.js
+++ src/controllers/mqttSubsController.js
@@ -1,6 +1,7 @@
 const mqtt = require('mqtt');
 const mongodbClient = require('mongodb').MongoClient;
-const mongoUri = "mongodb://mongocaio:m0ng0ldb*@clusteruno-shard-00-01-7t23t.mongodb.net:27017/test?ssl=true&replicaSet=ClusterUno-shard-0&authSource=admin";
+const mongoUri = "mongodb://mongocaio:m0ng0ldb*@clusteruno-shard-00-01-7t23t.mongodb.net:27017/maquinas?ssl=true&replicaSet=ClusterUno-shard-0&authSource=admin";
+
 
 function MqttSubsController(url, topic){
     let client = mqtt.connect("mqtt://"+url);
@@ -16,13 +17,21 @@ function MqttSubsController(url, topic){
             mongodbClient.connect(mongoUri, (err, db) => {
                 if (err) throw err;
                 //Faz uma query simples
-                db.collection('maquinas').find({modelo: msg.toString()}).toArray((error, results) => {
+                db.collection('maquinas').find({_id: JSON.parse(msg.toString())._id}).toArray((error, results) => {
                     if (error) throw error;
                     //Trata o array de resultados
                     results.map( (result, index) => {
-                        console.log('data:', result);
+                        console.log('->result:', result);
                     });
-
+                    db.close();
+                });
+                
+                mongodbClient.connect(mongoUri, (err, db) => {
+                    if (err) throw err;
+                    msg = JSON.parse(msg.toString());
+                    console.log('->msg.tempo_ligada:', msg.tempo_ligada);
+                    //Faz update do estado incrementando o tempo total que já passou ligada
+                    db.collection('maquinas').updateOne({"_id": msg._id}, {$set : {"ligada": msg.ligada}, $inc: {"tempo_ligada": msg.tempo_ligada}});
                     db.close();
                 });
             });
