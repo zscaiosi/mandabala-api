0cf9e2f52d97fddb27bbd8ebd5688f917df1710b
zscaiosi <zscaio.si@gmail.com>
2017-09-09 11:08:45 -0300

    Authentication

--- src/config/config.js
+++ src/config/config.js
@@ -1,20 +1,56 @@
-let express = require('express');
-let bodyParser = require('body-parser');
-let maquinas = require('../routes/routesMaquina');
-let clientes = require('../routes/routesCliente');
-let operadores = require('../routes/routesOperadores');
-const admins = require('../routes/routesAdmin');
-const cors = require('cors');
+let express = require("express");
+let bodyParser = require("body-parser");
+let maquinas = require("../routes/routesMaquina");
+let clientes = require("../routes/routesCliente");
+let operadores = require("../routes/routesOperadores");
+const admins = require("../routes/routesAdmin");
+const cors = require("cors");
+const passport = require("passport");
+const LocalStrategy = require('passport-local').Strategy;
+const User = require("../models/User");
+const usuario = new User();
 
-module.exports = function(){
-    let app = express();
-    app.use(cors());
-    app.use(bodyParser.urlencoded({extended: true}));
-    app.use(bodyParser.json());
-    app.use('/maquinas', maquinas);
-    app.use('/clientes', clientes);
-    app.use('/operadores', operadores);
-    app.use('/admins', admins);
+passport.serializeUser(function(user, done) {
+  done(null, user._id);
+});
 
-    return app;
-}
+passport.deserializeUser(function(id, done) {
+	const user = new User();
+	
+  use.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+passport.use(new LocalStrategy(
+	function(username, password, done) {
+
+
+		usuario.auth(username, password, function(error, user){
+
+			if(error){return done(error);}
+
+			if(!user){
+				done(null, false, {message: 'Não encontrado!'});
+			}
+
+			return done(null, user);
+
+		});
+	}
+));
+
+module.exports = function() {
+  let app = express();
+  app.use(cors());
+  app.use(bodyParser.urlencoded({ extended: true }));
+	app.use(bodyParser.json());
+	app.use(passport.initialize());
+	app.use(passport.session());
+  app.use("/maquinas", maquinas);
+  app.use("/clientes", clientes);
+  app.use("/operadores", operadores);
+  app.use("/admins", admins);
+
+  return app;
+};
